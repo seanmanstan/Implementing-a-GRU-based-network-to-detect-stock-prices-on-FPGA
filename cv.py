174716207d50c349e3ebc8b110e213296052e2d2
seanmanstan <89945664+seanmanstan@users.noreply.github.com>
2022-10-06 12:49:48 -0700

    Update cv.py

--- cv.py
+++ cv.py
@@ -2,8 +2,8 @@ import datetime as dt  # for working with dates
 import pandas as pd  # for working with large lists and large csv files
 from pandas_datareader import data as pdr  # for main stock data
 import csv  # for working with csv files
-start = datetime.now()
-end =datetime.now()
+start = dt.now()
+end =dt.now()
 tims=pdr.DataReader(start,end)
 filed="L.csv"
 with open(filed, 'w',encoding='UTF8') as f:  # this writes the current stock tickers data to a csv file
