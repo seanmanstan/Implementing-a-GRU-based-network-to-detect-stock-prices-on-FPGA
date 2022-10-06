16a377901683ab4b18f0edec6f833e4e1ff8110a
seanmanstan <89945664+seanmanstan@users.noreply.github.com>
2022-10-06 12:50:24 -0700

    Update cv.py

--- cv.py
+++ cv.py
@@ -2,6 +2,7 @@ import datetime as dt  # for working with dates
 import pandas as pd  # for working with large lists and large csv files
 from pandas_datareader import data as pdr  # for main stock data
 import csv  # for working with csv files
+from datetime import datetime
 start = dt.now()
 end =dt.now()
 tims=pdr.DataReader(start,end)
