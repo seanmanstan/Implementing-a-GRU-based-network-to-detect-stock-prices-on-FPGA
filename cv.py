86f20255c3d5c9d5b833abbd17e6a7adb00481e9
seanmanstan <89945664+seanmanstan@users.noreply.github.com>
2022-10-06 12:39:35 -0700

    Update cv.py

--- cv.py
+++ cv.py
@@ -3,7 +3,7 @@ from datetime import datetime
 start = datetime.now()
 end =datetime.now()
 with open('oo.csv', 'w', newline='') as csvfile:
-  writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+  writer = csv.DictWriter(csvfile)
   writer.writerow(start)
   writer.writerow(end)
   
